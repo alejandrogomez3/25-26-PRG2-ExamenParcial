f504c3ee6163b94369b0e8366987bf356fc8e4c5
alejandrogomez3 <alejandro.gomez3@alumnos.uneatlantico.es>
2026-04-06 16:58:30 +0200

    Añadimos la vista pública

--- entregas/GómezAlejandro/src/usuariobiblioteca.java
+++ entregas/GómezAlejandro/src/usuariobiblioteca.java
@@ -1,1 +1,12 @@
+class usuariobiblioteca {
 
+    public usuariobiblioteca(String usuario, String DNI, String direccion);
+
+    public void prestamo(double cantidad);
+
+    public void devoluciones(double cantidad);
+
+    public double consultarPrestamos();
+
+    public String consultarHistorial();
+}
