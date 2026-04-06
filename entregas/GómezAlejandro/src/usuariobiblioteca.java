5626e12be9fcd1a02054e5ea64afa6c4eab5189a
alejandrogomez3 <alejandro.gomez3@alumnos.uneatlantico.es>
2026-04-06 16:58:53 +0200

    Añadimos la vista privada

--- entregas/GómezAlejandro/src/usuariobiblioteca.java
+++ entregas/GómezAlejandro/src/usuariobiblioteca.java
@@ -10,3 +10,44 @@ class usuariobiblioteca {
 
     public String consultarHistorial();
 }
+public class usuariobiblioteca {
+
+    private String usuario;
+    private String DNI;
+    private String direccion;
+    private double prestamos;
+    private String historial;
+
+    public usuariobiblioteca(String usuario, String DNI, String direccion) {
+        this.usuario = usuario;
+        this.DNI = DNI;
+        this.direccion = direccion;
+        this.prestamos = 0;
+        this.historial = "";
+    }
+
+    public void prestamo(double cantidad) {
+        if (cantidad > 0) {
+            prestamos = prestamos + cantidad;
+            historial = historial + "Préstamo de " + cantidad + "\n";
+        }
+    }
+
+    public void devoluciones(double cantidad) {
+        if (cantidad > 0) {
+            prestamos = prestamos - cantidad;
+            if (prestamos < 0) {
+                prestamos = 0;
+            }
+            historial = historial + "Devolución de " + cantidad + "\n";
+        }
+    }
+
+    public double consultarPrestamos() {
+        return prestamos;
+    }
+
+    public String consultarHistorial() {
+        return historial;
+    }
+}
